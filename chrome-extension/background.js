cdc1af741e7ec880d80e7d5b1480fa6334350a42
VelmaScooby <monkeyprofessioanal8@gmail.com>
2019-05-31 04:01:33 -0400

    add .pyc to gitignore

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,8 +1,60 @@
 
+
+//JUST AN EXAMPLE, PLEASE USE YOUR OWN PICTURE!
+var imageAddr = "http://www.kenrockwell.com/contax/images/g2/examples/31120037-5mb.jpg"; 
+var downloadSize = 4995374; //bytes
+
+
+function ShowProgressMessage(bandwidth) {
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        geoloc = JSON.stringify(xhr.responseText,null,2);
+
+        var xhr1 = new XMLHttpRequest();
+        xhr1.open("POST", 'http://127.0.0.1:8000/schools', true);
+        xhr1.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr1.send(JSON.stringify({"bandwidth": bandwidth, "school": geoloc}));
+    }
+  } 
+  xhr.open("GET", 'http://ip-api.com/json?', true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(null);
+  
+}
+
+function MeasureConnectionSpeed() {
+    var startTime, endTime;
+    var download = new Image();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        showResults();
+    }
+    
+    download.onerror = function (err, msg) {
+      showResults();
+    }
+    
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    download.src = imageAddr + cacheBuster;
+    
+    function showResults() {
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 8;
+        var speedBps = (bitsLoaded / duration).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
+        ShowProgressMessage(speedKbps);
+    }
+}
+
+
 setInterval(
   function(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://unicef-isp-manager.ngrok.io/schools', true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(JSON.stringify({ "ip": "172.20.5.142"}));
- }, 30000);
+    MeasureConnectionSpeed();
+ }, 3000);
+
+
+
--- server-django/unicef/connectivity/models.py
+++ server-django/unicef/connectivity/models.py
@@ -4,16 +4,37 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Prob(models.Model):
+  bandwidth = models.FloatField()
+  timestamp = models.DateTimeField(auto_now_add=True)
+  school = models.ForeignKey('School', on_delete=models.CASCADE)
+  isp = models.ForeignKey('Isp', on_delete=models.CASCADE)
+
+  def __str__(self): 
+    return self.timestamp
 
 class Isp(models.Model):
-  address = models.CharField(max_length=40, unique=True)
+  address = models.CharField(max_length=40)
+  name = models.CharField(max_length=120, null=True)
   scrupulous = models.BooleanField()
 
+  def __str__(self): 
+    return self.name
+
 class School(models.Model):
-  name = models.CharField(max_length=60)
-  ip = models.CharField(max_length=16)
-  connectivity = models.IntegerField()
-  Isp = models.ForeignKey('Isp', on_delete=models.SET_NULL, null=True)
+  ip = models.CharField(max_length=40, null=True)
+  org = models.CharField(max_length=120, null=True)
+  city = models.CharField(max_length=120, null=True)
+  region = models.CharField(max_length=120, null=True)
+  country = models.CharField(max_length=120, null=True)
+  zip = models.CharField(max_length=40, null=True)
+  lat = models.FloatField(default=0.0)
+  lon = models.FloatField(default=0.0)
+  timezone = models.CharField(max_length=120, null=True)
+
+  def __str__(self):
+    return self.city +", " + self.region
+
 
 
 
